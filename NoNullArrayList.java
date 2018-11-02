ca72e32755c10c7d3b9e2df7198994e7ab413096
dramsammy <dramsammy@stuy.edu>
2018-11-02 11:28:15 -0400

    Finished all of methods for NoNullArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,7 +6,22 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
-  public boolean add(){
-
+  public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    return super.add(element);
+    }
+  public void add(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    super.add(index, element);
+    }
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    return super.set(index, element);
+    }
   }
-}
