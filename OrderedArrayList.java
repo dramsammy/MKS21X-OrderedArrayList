f2b26f0dd7ef67557732ac51b79fc11bf1215fb7
Dramsammy <dramsammy@stuy.edu>
2018-11-03 21:14:24 -0400

    Almost finished ordered array list

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -6,4 +6,29 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
+  public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < this.size(); i++){
+      if ((element.compareTo(this.get(i))) > 0){
+        super.add(i, element);
+      }
+      else {
+        super.add(i-1,element);
+    }
+  }
+    return super.add(element);
+  }
+  public void add(int index, T element){
+    add(element);
+  }
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException();
+    }
+    T returnValue = super.remove(index);
+    add(element);
+    return returnValue;
+  }
 }
